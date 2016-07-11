2945ef00ef89bd977d904577a7d2c178063ee03c
Alexander Käßner <alexkaessner@me.com>
2016-07-11 17:04:07 +0200

    added dotted map overlay circles with data

--- js/dotted-world-map.js
+++ js/dotted-world-map.js
@@ -4,3 +4,42 @@ d3.xml("graphics/dotted-worldmap.svg").mimeType("image/svg+xml").get(function(er
   if (error) throw error;
   document.getElementById("dotted-world-map").appendChild(xml.documentElement);
 });
+
+// BASIC VARIABLES -------------------------------------------------------------
+var cityNames = ["New York", "London", "Mumbai", "Shanghai", "Rio de Janairo", "Johannesburg", "sydney"];
+var cityLocations = [[27, 21], [60, 14], [93, 31], [113, 24], [40, 51], [72, 54], [128, 56]];
+var mapScaling = 8;
+var populationFactor = 1000000; // Million
+var populationScaling = 2.3;
+
+// -----------------------------------------------------------------------------
+// load the JSON file and draw the circles
+d3.json("data/urbanisation.json", function(error, data){
+	if (error) return console.error(error);
+
+	// basic setup of the SVG
+	var svg = d3.select('#dotted-world-map').append('svg')
+	    .attr('width', 1140)
+	    .attr('height', 542)
+			.style("position", "absolute");
+
+	var cityCircle = svg.selectAll("circle")
+			.data(data)
+			.enter();
+
+	cityCircle.append("circle")
+				.attr("class", "y2030")
+				.attr("cx", function(d){ return (d.location[0] * mapScaling); })
+				.attr("cy", function(d){ return (d.location[1] * mapScaling); })
+				.attr("r", function(d){ return ((d.y2030 / populationFactor) * populationScaling); });
+	cityCircle.append("circle")
+				.attr("class", "y1990")
+				.attr("cx", function(d){ return (d.location[0] * mapScaling); })
+				.attr("cy", function(d){ return (d.location[1] * mapScaling); })
+				.attr("r", function(d){ return ((d.y1990 / populationFactor) * populationScaling); });
+	cityCircle.append("circle")
+				.attr("class", "y1950")
+				.attr("cx", function(d){ return (d.location[0] * mapScaling); })
+				.attr("cy", function(d){ return (d.location[1] * mapScaling); })
+				.attr("r", function(d){ return ((d.y1950 / populationFactor) * populationScaling); });
+});
